f9fb531458bc76f1b0bf30861c4ba5ee44719be6
JoseP-Dm <60919850+JoseP-Dm@users.noreply.github.com>
2020-02-11 10:42:56 +0100

    Add files via upload

---
+++ Ex_26.java
@@ -0,0 +1,45 @@
+
+package ex_26;
+
+import java.util.Scanner;
+
+public class Ex_26 {
+
+    public static void main(String[] args) {
+            Scanner lector = new Scanner(System.in);
+            int n1=0,n2=0,contador=0,resultat=0;
+            boolean  correcte=false;
+      
+            
+            do{
+        System.out.print("Escriu el primer numero: ");
+                if (lector.hasNextInt()) 
+                    {
+                n1= lector.nextInt();
+                                                
+                correcte=true;
+               } else {
+                
+                correcte=false;
+                    lector.nextLine();
+                            }
+                System.out.print("Escriu el segon numero: ");
+                if (lector.hasNextInt()) 
+                    {
+                n2=lector.nextInt();
+                               
+                correcte=true;
+               } else {
+                
+                correcte=false;
+                    lector.nextLine();
+                            }
+          }while((n1<=0)||(n2<=0)||(!correcte));
+            
+            for(contador=1;contador<=n2;contador++){
+                resultat= n1*n1;
+            }
+            System.out.println("El resultat es: "+resultat);
+            System.out.println("easy peasy ")
+         }
+}
